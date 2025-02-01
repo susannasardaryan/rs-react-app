11003ab4daa23e6afcf389d4969bcaad1887dc20
susannasardaryan <susannasardaryann@gmail.com>
2025-02-02 02:10:32 +0400

    fix: issue with errorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,40 @@ import './App.css'
 import Search from './components/Search.tsx'
 import Results from './components/Results.tsx'
 import { Component } from 'react'
-import ErrorBoundary from './components/ErrorBoundary.tsx'
+
 type State = {
-  searchTerm: String
+  searchTerm: String,
+  hasError: boolean
 }
-class App extends Component<{},State> {
-constructor(props:{}){
-  super(props)
-  this.state = {
-    searchTerm: localStorage.getItem('searchTerm') || ''
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      searchTerm: localStorage.getItem('searchTerm') || '',
+      hasError: false
+    }
   }
-}
 
-handleSearch(value:string){
-  this.setState({ searchTerm: value });
-  localStorage.setItem('searchTerm', value);
-}
-throwError(){
-  throw new Error('I crashed!');
-}
-render():JSX.Element{
-  return (
-    <ErrorBoundary>
-      <Search onSearch={ this.handleSearch.bind(this)}></Search>
-      <Results searchTermValue={this.state.searchTerm}></Results>
-      <button onClick={this.throwError.bind(this)}>Error button</button>
-    </ErrorBoundary>
+  handleSearch(value: string) {
+    this.setState({ searchTerm: value });
+    localStorage.setItem('searchTerm', value);
+  }
+  throwError() {
+    this.setState({ hasError: true })
+  }
+  render(): JSX.Element {
+    if (this.state.hasError) {
+      throw new Error('I crashed!');
+    }
 
-  )
-}
+    return (
+      <>
+        <Search onSearch={this.handleSearch.bind(this)}></Search>
+        <Results searchTermValue={this.state.searchTerm}></Results>
+        <button onClick={this.throwError.bind(this)}>Error button</button>
+      </>
+    )
+  }
 
 }
 
--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,29 +1,36 @@
-import { Component, ErrorInfo} from "react"
+import { Component, ErrorInfo } from "react";
 
 type State = {
-    hasError: boolean
+  error: Error | null;
+  errorInfo: ErrorInfo | null;
+};
+
+class ErrorBoundary extends Component<React.PropsWithChildren, State> {
+  constructor(props: React.PropsWithChildren) {
+    super(props);
+    this.state = {
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    this.setState({ error: error, errorInfo: errorInfo });
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return (
+        <div>
+          <h1>Something went wrong.</h1>
+          <p>{this.state.error?.message}</p>
+          <pre>{this.state.errorInfo.componentStack}</pre>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
 }
-class ErrorBoundary extends Component<React.PropsWithChildren<{}>, State> {
-    constructor(props: React.PropsWithChildren) {
-        super(props);
-        this.state = {
-            hasError: false
-        }
-    }
 
-    componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-        this.setState({hasError : true});
-        console.error(error, errorInfo);
-    }
-    
-
-    render() {
-        if(this.state.hasError){
-            return <h1>There is something wrong</h1>
-        }
-
-        return this.props.children
-    }
-}
-
-export default ErrorBoundary
+export default ErrorBoundary;
